03ef9daa7dd43bf98c12257cf7c955dfc60bc804
AbhayRai <raiabhay002@gmail.com>
2024-02-05 11:55:13 +0530

    Add files via upload

---
+++ e4_redux-main/e4_redux-main/src/App.js
@@ -0,0 +1,16 @@
+ import React from 'react'
+import Counter from './component/Counter'
+import Todo from './component/Todo'
+import ControllandUncontroll from './component/ControllandUncontroll'
+import ControllandUncontrollCounter from './component/ControllandUncontrollCounter'
+ 
+ function App() {
+   return (
+      // <Counter/>
+      // <Todo/>
+      // <ControllandUncontroll/>
+      <ControllandUncontrollCounter/>
+   )
+ }
+ 
+ export default App
